9b5018079d80c4bbcb50dcd55c17d8f4f6ebcc4a
Najib <najib.muqeem@gmail.com>
2020-05-18 12:08:16 -0400

    db setup, seeds

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (username) VALUES ('najib');
+
+INSERT INTO polls (user_id, query, number_of_options) VALUES (1, "What is your favorite color among the following?", 4)
+
+INSERT INTO options (poll_id, text) VALUES (1, "Blue")
+INSERT INTO options (poll_id, text) VALUES (1, "Green")
+INSERT INTO options (poll_id, text) VALUES (1, "Yellow")
+INSERT INTO options (poll_id, text) VALUES (1, "Orange")
+INSERT INTO options (poll_id, text) VALUES (1, "Red")
